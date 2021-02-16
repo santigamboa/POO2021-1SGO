2b5b4f418b7440886b789f07a2396fed99a5c01d
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-02-16 17:24:58 -0500

    Se crea el main

---
+++ Calentamiento/IVA.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float precio, precio_mod;
+    cout << "Ingrese el precio del producto: ";
+    cin >> precio;
+    
+    return 0;
+}
