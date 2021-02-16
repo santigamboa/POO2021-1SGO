bd889269d5af9eb8fdbf2a2b88eefd54f9c20943
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-02-16 17:57:04 -0500

    Se crea e integra la función leerDatos

--- Calentamiento/Datos_persona.cpp
+++ Calentamiento/Datos_persona.cpp
@@ -11,7 +11,20 @@ struct persona{
 
 typedef struct persona persona;
 
+persona leerDatos(){
+    persona x;
+    cout << "Ingrese la edad de la persona: ";
+    cin >> x.edad;
+    cout << "Ingrese el sexo de la persona (m/f): ";
+    cin >> x.sexo;
+    cout << "Ingrese la altura de la persona (en metros): ";
+    cin >> x.altura;
+    return x;
+}
+
 int main(){
+    persona x;
+    x = leerDatos();
     
     return 0;
 }
