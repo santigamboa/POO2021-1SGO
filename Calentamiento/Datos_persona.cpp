22f73a67efb53173135a7eb2a450d00b22e9b276
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-02-16 17:47:34 -0500

    Se crea el main y el struct persona

---
+++ Calentamiento/Datos_persona.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+struct persona{
+    int edad;
+    char sexo;
+    float altura;
+};
+
+typedef struct persona persona;
+
+int main(){
+    
+    return 0;
+}
