cbdcc2ffdc2465fae14e1007a9269434ba6bef58
Santiago Gamboa <santigamboa@javerianacali.edu.co>
2021-02-16 17:28:16 -0500

    Se crea e integra la función agregarIVA

--- Calentamiento/IVA.cpp
+++ Calentamiento/IVA.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+#define IVA 0.19
 
 using namespace std;
+
+float agregarIva( float precio ){
+    return precio*( 1 + IVA );
+}
 
 int main(){
     float precio, precio_mod;
     cout << "Ingrese el precio del producto: ";
     cin >> precio;
-    
+    precio_mod = agregarIva( precio );
+    cout << "El precio del producto con IVA incluido es: " << precio_mod << endl;
     return 0;
 }
